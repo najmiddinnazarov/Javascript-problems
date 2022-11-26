394df0c8db80264abf0a93714faba795bf164462
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-26 11:56:07 +0500

    Add 3 issues

--- index.js
+++ index.js
@@ -106,6 +106,22 @@
 // }
 // console.log(sum);
 
+// No8
+// let arr = [
+//   { id: 1, name: "Usmon", job: "developer" },
+//   { id: 2, name: "Usmon", job: "developer" },
+//   { id: 3, name: "Usmon" },
+//   { id: 4, name: "Usmon", job: "developer" },
+//   { id: 5, name: "Usmon" },
+// ];
+// for (let i = 0; i < arr.length; i++) {
+//   for (const key in arr[i]) {
+//     if (key == "job") {
+//       console.log(arr[i]);
+//     }
+//   }
+// }
+
 // No9
 // let obj = {
 //   id: 1,
@@ -114,3 +130,66 @@
 // };
 // let newObj = structuredClone(obj);
 // console.log(newObj);
+
+// No11
+// let obj = {
+//   id: 1,
+//   name: "Usmon",
+//   job: "developer",
+//   practice: 1,
+// };
+// for (const key in obj) {
+//   if (key == "practice") {
+//     if (obj[key] >= 1) {
+//       console.log("Qabul qilindi");
+//     } else {
+//       console.log("Ming bor uzr");
+//     }
+//   }
+// }
+
+// No12
+// let obj = {
+//   id: 1,
+//   name: "Usmon",
+//   job: "developer",
+//   practice: 1,
+//   talaba: true,
+// };
+// for (const key in obj) {
+//   if (key == "talaba") {
+//     if (obj[key]) {
+//       obj.credit = true;
+//     } else {
+//       obj.credit = false;
+//     }
+//   }
+// }
+// console.log(obj);
+
+// No13
+// object ichida tugilgan yil,hozirgi yoshi malumotlari keltirilgan. agar yosh togri bolmasa 'xato malumot kiritdingiz' chiqarilsin
+// let nowYear = 2022;
+// let obj = {
+//   id: 1,
+//   name: "Usmon",
+//   age: 32,
+//   year: 1990,
+// };
+// for (const key in obj) {
+//   let newAge = "";
+//   if (key == "year") {
+//     Number((newAge += nowYear - obj[key]));
+//     // if (key == "age") {
+//     //   if (newAge) {
+//     //     console.log(newAge);
+//     //   } else {
+//     //   }
+//     //   // console.log(obj[key]);
+//     // }
+//   }
+//   console.log(newAge);
+// }
+// console.log(newAge);
+// chiqish
+// 'xato malumot kiritdingiz'
