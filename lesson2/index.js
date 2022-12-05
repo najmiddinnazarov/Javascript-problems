79e0e3abac15dfa3e42c4fb9748affad96c532c4
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-12-05 21:01:17 +0500

    Add 1 issue

---
+++ lesson2/index.js
@@ -0,0 +1,25 @@
+// No1
+// let str = "Webbrain Academy";
+// let arr = str.split("");
+// let upperCaseLetter = 0;
+// let lowerCaseLetter = 0;
+// let result = arr.filter((value) => {
+//   if (value.toLowerCase() !== value.toUpperCase()) {
+//     if (value == value.toUpperCase()) {
+//       return upperCaseLetter++;
+//     } else {
+//       return lowerCaseLetter++;
+//     }
+//   }
+// });
+// console.log(`${upperCaseLetter} ta katta harf.
+// ${lowerCaseLetter} ta kichik harf.`);
+
+// No2
+// let str = "WebBr@in";
+// let arr = str.split(" ");
+// let result = "";
+// for (const i of arr) {
+//   result += arr.splice(i.indexOf("@"), 3, "a");
+// }
+// console.log(result); // Error
